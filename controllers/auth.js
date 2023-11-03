8d4801f36e7d9a682cab1d1c26b41fbcf7b53cb6
abdullahdgrey8 <abdullahkhalid204606@gmail.com>
2023-11-03 16:04:53 +0500

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json(req.body);
+};
+const login = async (req, res) => {
+  res.send("login user ");
+};
+module.exports = {
+  register,
+  login,
+};
